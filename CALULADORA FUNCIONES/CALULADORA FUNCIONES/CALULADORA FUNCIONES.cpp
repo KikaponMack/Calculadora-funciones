1cdd96c6a207a2f955f38e8c8802504c1f3ed6b2
LUCHI <Alumno@PC34>
2019-10-31 12:21:50 -0600

    Hasta que me abrió el visual >:I

---
+++ CALULADORA FUNCIONES/CALULADORA FUNCIONES/CALULADORA FUNCIONES.cpp
@@ -0,0 +1,116 @@
+#include <string>
+#include <iostream>
+using namespace std;
+//Declaramos prototipos de funciones
+void menu();
+int suma();
+int resta();
+int multi();
+int division();
+void pausa();
+int main()
+{
+	menu();
+	return 0;
+}
+//creamos funciones
+void menu()
+{
+	bool bandera = false;
+	char tecla;
+	do
+	{
+		system("cls");
+		cin.clear();
+		cout << "Calculadora con funciones" << endl;
+		cout << "-------------------------" << endl;
+		cout << "\t1.- Sumar" << endl;
+		cout << "\t2.- Restar" << endl;
+		cout << "\t3.- Multiplicar" << endl;
+		cout << "\t4.- Dividir" << endl;
+		cout << "\t5.- Salir" << endl;
+		cout << "Elige una opcion: " << endl;
+		cin >> tecla;
+		switch (tecla)
+		{
+		case '1':
+			system("cls");
+			suma();
+			break;
+		case '2':
+			system("cls");
+			resta();
+			break;
+		case '3':
+			system("cls");
+			multi();
+			break;
+		case '4':
+			system("cls");
+			division();
+			break;
+		case '5':
+			bandera = true;
+			break;
+		default:
+			system("cls");
+			cout << "Opcion no valida" << endl;
+			pausa();
+			break;
+		}
+	} while (bandera != true);
+}
+void pausa()
+{
+	cout << endl << "Pulsa cualquier tecla para salir";
+	getwchar();
+	getwchar();
+}
+int suma()
+{
+	int a, b, suma;
+	cout << "Primer valor:";
+	cin >> a;
+	cout << "Segundo valor:";
+	cin >> b;
+	suma = a + b;
+	cout << "La suma es:" << suma;
+	pausa();
+	return 0;
+}
+int resta()
+{
+	int c, d, resta;
+	cout << "Primer valor:";
+	cin >> c;
+	cout << "Segundo valor:";
+	cin >> d;
+	resta = c - d;
+	cout << "La resta es:" << resta;
+	pausa();
+	return 0;
+}
+int multi()
+{
+	int e, f, multi;
+	cout << "Primer valor:";
+	cin >> e;
+	cout << "Segundo valor:";
+	cin >> f;
+	multi = e * f;
+	cout << "La multi es:" << multi;
+	pausa();
+	return 0;
+}
+int division()
+{
+	int a, b, division;
+	cout << "Primer valor:";
+	cin >> a;
+	cout << "Segundo valor:";
+	cin >> b;
+	division = a / b;
+	cout << "La division es:" << division;
+	pausa();
+	return 0;
+}
